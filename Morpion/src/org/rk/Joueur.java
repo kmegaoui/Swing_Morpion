5a461cb0ee174f1f73ed3a2a1bfc7bc93b1dc194
kmegaoui <kevin.megaoui@gmail.com>
2014-05-15 15:44:23 +0200

    ajout boolean pour les tour de jeux

--- Morpion/src/org/rk/Joueur.java
+++ Morpion/src/org/rk/Joueur.java
@@ -4,10 +4,12 @@ package org.rk;
 public class Joueur
 {
 	private String nom;
+	private boolean estAutoriseAJouer;
 
 	public Joueur(String n)
 	{
 		this.nom = n;
+		this.estAutoriseAJouer = false;
 	}
 
 	public String toString()
