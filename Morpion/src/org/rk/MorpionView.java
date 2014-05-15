efe3de20a5e9dcbd7a5d9eb5f1fd3f58720b9982
Locron <rethoreronan@free.fr>
2014-05-15 16:07:11 +0200

    Finnition de l'en-tête

--- Morpion/src/org/rk/MorpionView.java
+++ Morpion/src/org/rk/MorpionView.java
@@ -2,14 +2,17 @@
 package org.rk;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 /**
@@ -20,8 +23,10 @@ public class MorpionView extends Observable implements Observer
 {
 	MorpionModel mm;
 	JFrame frame;
-	JPanel jpNorth, jpEast;
-	GridBagLayout gblHead;
+	JPanel jpNord, jpEst;
+	GridBagLayout gblEnTete;
+	JTextField jtNom;
+	JButton jbCreer, jbRejoindre;
 
 	public MorpionView(MorpionModel mm)
 	{
@@ -34,18 +39,40 @@ public class MorpionView extends Observable implements Observer
 			{
 				frame = new JFrame("Morpion RK");
 				frame.getContentPane().setLayout(new BorderLayout());
-				jpNorth = new JPanel();
-				gblHead = new GridBagLayout();
-				jpNorth.setLayout(gblHead);
+				jpNord = new JPanel();
+				// jpNord.setPreferredSize(new Dimension(5, 20));
+				gblEnTete = new GridBagLayout();
+				jpNord.setLayout(gblEnTete);
 				GridBagConstraints gbc = new GridBagConstraints();
+
+				JLabel jlNom = new JLabel("Nom : ");
 				gbc.gridheight = 2;
 				gbc.gridx = 0;
 				gbc.gridy = 0;
-				JLabel jlNom = new JLabel("Nom : ");
-				jpNorth.add(jlNom, gbc);
+				jpNord.add(jlNom, gbc);
 
-				frame.add(jpNorth, BorderLayout.NORTH);
+				jtNom = new JTextField();
+				jtNom.setPreferredSize(new Dimension(100, 20));
+				gbc.gridx = 1;
+				jpNord.add(jtNom, gbc);
 
+				jbCreer = new JButton("Créer");
+				jbCreer.setPreferredSize(new Dimension(100, 20));
+				gbc.gridheight = 1;
+				gbc.gridx = 2;
+				gbc.gridy = 0;
+				jpNord.add(jbCreer, gbc);
+
+				jbRejoindre = new JButton("Créer");
+				jbRejoindre.setPreferredSize(new Dimension(100, 20));
+				gbc.gridheight = 1;
+				gbc.gridx = 2;
+				gbc.gridy = 1;
+				jpNord.add(jbRejoindre, gbc);
+
+				// Ajout du panel entête au nord de la frame
+				frame.add(jpNord, BorderLayout.NORTH);
+				// Affichage de la Frame
 				frame.pack();
 				frame.setVisible(true);
 			}
