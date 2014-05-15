0b871721542fd69e57ebfee19e86dc97a2a6d579
Locron <rethoreronan@free.fr>
2014-05-15 15:49:17 +0200

    Ajout frame principal et panel avec gridbaglayout pour le Head de la fenêtre.

--- Morpion/src/org/rk/MorpionView.java
+++ Morpion/src/org/rk/MorpionView.java
@@ -1,10 +1,16 @@
 
 package org.rk;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
  * @author ronan
@@ -12,11 +18,38 @@ import javax.swing.JFrame;
  */
 public class MorpionView extends Observable implements Observer
 {
+	MorpionModel mm;
 	JFrame frame;
+	JPanel jpNorth, jpEast;
+	GridBagLayout gblHead;
 
 	public MorpionView(MorpionModel mm)
 	{
-		// TODO Auto-generated constructor stub
+		this.mm = mm;
+		SwingUtilities.invokeLater(new Runnable()
+		{
+
+			@Override
+			public void run()
+			{
+				frame = new JFrame("Morpion RK");
+				frame.getContentPane().setLayout(new BorderLayout());
+				jpNorth = new JPanel();
+				gblHead = new GridBagLayout();
+				jpNorth.setLayout(gblHead);
+				GridBagConstraints gbc = new GridBagConstraints();
+				gbc.gridheight = 2;
+				gbc.gridx = 0;
+				gbc.gridy = 0;
+				JLabel jlNom = new JLabel("Nom : ");
+				jpNorth.add(jlNom, gbc);
+
+				frame.add(jpNorth, BorderLayout.NORTH);
+
+				frame.pack();
+				frame.setVisible(true);
+			}
+		});
 	}
 
 	@Override
