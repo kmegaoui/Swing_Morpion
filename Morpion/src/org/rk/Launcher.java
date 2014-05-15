6c6198f8e6d0a99159bafe814a9cd3278adbff14
kmegaoui <kevin.megaoui@gmail.com>
2014-05-15 14:05:55 +0200

    Init projet

---
+++ Morpion/src/org/rk/Launcher.java
@@ -0,0 +1,17 @@
+
+package org.rk;
+
+public class Launcher
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Morpion/src/org/rk/MorpionModel.java
@@ -0,0 +1,6 @@
+package org.rk;
+
+public class MorpionModel
+{
+
+}
