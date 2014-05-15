20c014ee7e01a5d060f6902c25a153c2c0721f1e
kmegaoui <kevin.megaoui@gmail.com>
2014-05-15 14:43:00 +0200

    ajout model et case

--- Morpion/src/org/rk/MorpionModel.java
+++ Morpion/src/org/rk/MorpionModel.java
@@ -1,6 +1,29 @@
+
 package org.rk;
 
-public class MorpionModel
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+public class MorpionModel extends Observable
 {
+	private long nbLigne;
+	private long nbColonne;
+	List<Joueur> listeJoueur;
+	Case[][] grille;
+
+	public MorpionModel()
+	{
+		initStandard();
+	}
+
+	public void initStandard()
+	{
+		this.nbLigne = 3;
+		this.nbColonne = 3;
+		this.listeJoueur = new ArrayList<Joueur>();
+		listeJoueur.add(new Joueur("Joueur 1"));
+		listeJoueur.add(new Joueur("Joueur 2"));
+	}
 
 }
