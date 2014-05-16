d2e8531df45ddbdae9461205189bb3bfed1bc87b
kmegaoui <kevin.megaoui@gmail.com>
2014-05-16 10:27:41 +0200

    maj Joueur : ajout liste resultats maj Model : ajout methode pour coup gagnant

--- Morpion/src/org/rk/MorpionModel.java
+++ Morpion/src/org/rk/MorpionModel.java
@@ -1,34 +1,35 @@
 
 package org.rk;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
 
 public class MorpionModel extends Observable
 {
 	private int nbLigne;
 	private int nbColonne;
-	private List<Joueur> listeJoueur;
+	private Joueur joueur1;
+	private Joueur joueur2;
 	private Case[][] grille;
 	private int nbCoupPossible;
 
 	public MorpionModel()
 	{
-		initStandard();
+		initJeuStandard();
 	}
 
-	public void initStandard()
+	public void initJeuStandard()
 	{
 		nbLigne = 3;
 		nbColonne = 3;
 		nbCoupPossible = nbLigne * nbColonne;
 
-		listeJoueur = new ArrayList<Joueur>();
-		listeJoueur.add(new Joueur("Joueur 1"));
-		listeJoueur.add(new Joueur("Joueur 2"));
+		joueur1 = new Joueur("Joueur 1");
+		joueur2 = new Joueur("Joueur 2");
+
+		initGrille();
 	}
 
+	// Preparation grille de jeu
 	public void initGrille()
 	{
 		grille = new Case[nbLigne][nbColonne];
@@ -42,6 +43,7 @@ public class MorpionModel extends Observable
 		}
 	}
 
+	// Action de jeu d'un joueur
 	public boolean jouerCoup(int ligne, int colonne, Joueur joueur)
 	{
 		if (joueur.estAutoriseAJouer())
@@ -67,19 +69,194 @@ public class MorpionModel extends Observable
 				&& colonne <= nbColonne;
 	}
 
-	public boolean estCoupGagnant()
-	{
-		return false;
-	}
-
 	public boolean estDernierCoup()
 	{
-		return false;
+		return nbCoupPossible <= 0;
 	}
 
 	public void decrementeCoupPossible()
 	{
 		nbCoupPossible--;
+	}
+
+	public boolean estCoupGagnant(int ligne, int colonne, Joueur joueur)
+	{
+		int ligneMin = ligne - 2;
+		if (ligneMin < 1)
+			ligneMin = 1;
+
+		int ligneMax = ligne + 2;
+		if (ligneMax > nbLigne)
+			ligneMax = nbLigne;
+
+		int colonneMin = ligne - 2;
+		if (colonneMin < 1)
+			colonneMin = 1;
+
+		int colonneMax = ligne + 2;
+		if (colonneMax > nbColonne)
+			colonneMax = nbColonne;
+
+		int cpt;
+
+		// Verification des lignes potentiellement gagnantes
+		for (int i = ligneMin; i < ligneMax; i++)
+		{
+			cpt = 0;
+			for (int j = colonneMin; j < colonneMax; j++)
+			{
+				if (joueur.equals(grille[ligne - 1][colonne - 1].getJoueur()))
+				{
+					cpt++;
+				}
+				else
+				{
+					cpt = 0;
+				}
+			}
+			if (cpt == 3)
+			{
+				return true;
+			}
+		}
+
+		// Verification des colonnes potentiellement gagnantes
+		for (int i = colonneMin; i < colonneMax; i++)
+		{
+			cpt = 0;
+			for (int j = ligneMin; j < ligneMax; j++)
+			{
+				if (joueur.equals(grille[ligne - 1][colonne - 1].getJoueur()))
+				{
+					cpt++;
+				}
+				else
+				{
+					cpt = 0;
+				}
+			}
+			if (cpt == 3)
+			{
+				return true;
+			}
+		}
+
+		// Verification premiere diagonale
+		int indexLigne = ligneMin;
+		int indexColonne = colonneMin;
+		cpt = 0;
+
+		while (indexLigne <= ligneMax && indexColonne <= colonneMax)
+		{
+			if (joueur.equals(grille[indexLigne - 1][indexColonne - 1]
+					.getJoueur()))
+			{
+				cpt++;
+			}
+			else
+			{
+				cpt = 0;
+			}
+
+			indexLigne++;
+			indexColonne++;
+
+			if (cpt == 3)
+			{
+				return true;
+			}
+		}
+
+		// Verification seconde diagonale
+		indexLigne = ligneMax;
+		indexColonne = colonneMin;
+		cpt = 0;
+
+		while (indexLigne >= ligneMin && indexColonne <= colonneMax)
+		{
+
+			if (joueur.equals(grille[indexLigne - 1][indexColonne - 1]
+					.getJoueur()))
+			{
+				cpt++;
+			}
+			else
+			{
+				cpt = 0;
+			}
+
+			indexLigne--;
+			indexColonne++;
+
+			if (cpt == 3)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	// getters et setters
+
+	public int getNbLigne()
+	{
+		return nbLigne;
+	}
+
+	public void setNbLigne(int nbLigne)
+	{
+		this.nbLigne = nbLigne;
+	}
+
+	public int getNbColonne()
+	{
+		return nbColonne;
+	}
+
+	public void setNbColonne(int nbColonne)
+	{
+		this.nbColonne = nbColonne;
+	}
+
+	public Case[][] getGrille()
+	{
+		return grille;
+	}
+
+	public void setGrille(Case[][] grille)
+	{
+		this.grille = grille;
+	}
+
+	public int getNbCoupPossible()
+	{
+		return nbCoupPossible;
+	}
+
+	public void setNbCoupPossible(int nbCoupPossible)
+	{
+		this.nbCoupPossible = nbCoupPossible;
+	}
+
+	public Joueur getJoueur1()
+	{
+		return joueur1;
+	}
+
+	public void setJoueur1(Joueur joueur1)
+	{
+		this.joueur1 = joueur1;
+	}
+
+	public Joueur getJoueur2()
+	{
+		return joueur2;
+	}
+
+	public void setJoueur2(Joueur joueur2)
+	{
+		this.joueur2 = joueur2;
 	}
 
 }
